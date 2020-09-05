9cd3a25b0ba442a34183a0233e8774589173755d
Yan Vaidakovich <v.yan.v@yandex.ru>
2020-09-05 19:19:39 +0300

    build

---
+++ build/precache-manifest.12ab4f7cb1e43b2f10483f68b40907cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da44fa0c9dfe5d3935f06c91e7214cbd",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f8be48ec3693288e0523",
+    "url": "./static/css/main.5dd6c6e9.chunk.css"
+  },
+  {
+    "revision": "64c66f562d080cecb6d1",
+    "url": "./static/js/2.411a35a4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.411a35a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8be48ec3693288e0523",
+    "url": "./static/js/main.df6d2618.chunk.js"
+  },
+  {
+    "revision": "29f59fa2ec391eabb407",
+    "url": "./static/js/runtime-main.c695e3ae.js"
+  }
+]);
